0510e8ef4e5c459c8f1fe8418ea502edd68a0962
Ahlib <shahahlib@gmail.com>
2022-12-05 01:15:55 +0530

    Added more htmls

---
+++ main.js
@@ -0,0 +1,74 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "flex";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  
+  function orderOpen(){
+    document.getElementById("avi").style.display = "flex";
+  }
+  
+  function orderClose(){
+   document.getElementById("avi").style.display = "none";
+  }
+  
+  
+  function oO1(){
+    document.getElementById("o1").style.display = "flex";
+  }
+  
+  function oC1(){
+   document.getElementById("o1").style.display = "none";
+  }
+  
+  function oO2(){
+    document.getElementById("o2").style.display = "flex";
+  }
+  
+  function oC2(){
+   document.getElementById("o2").style.display = "none";
+  }
+  
+  function oO3(){
+    document.getElementById("o3").style.display = "flex";
+  }
+  
+  function oC3(){
+   document.getElementById("o3").style.display = "none";
+  }
+  
+  function oO4(){
+    document.getElementById("o4").style.display = "flex";
+  }
+  
+  function oC4(){
+   document.getElementById("o4").style.display = "none";
+  }
+  
+  
+  function trainB(){
+    document.getElementById("bookTrain").style.display = "flex";
+  }
+  
+  function trainC(){
+    document.getElementById("bookTrain").style.display = "none";
+  }
+  
+  function hpackage(){
+    document.getElementById("hBook").style.display = "flex";
+    
+  }
+  function orderO(){
+    document.getElementById("openO").style.display = "flex";
+  }
+  
+  function orderC(){
+   document.getElementById("openO").style.display = "none";
+  }
+  function hpackclose(){
+    document.getElementById("hBook").style.display = "none";
+  }
+  
+  
